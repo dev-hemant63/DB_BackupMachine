d9f460cb5335a29679fbd29b086bb9a8d83fa40a
Hemant Nag <104633997+dev-hemant63@users.noreply.github.com>
2024-06-18 16:38:04 +0530

    done

---
+++ FSM-master/Program.cs
@@ -0,0 +1,14 @@
+﻿using FSM.Repository;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DatabaseBackupStateMachine fsm = new DatabaseBackupStateMachine();
+
+        fsm.RequestBackup();
+
+        Console.ReadLine();
+    }
+}
--- FSM-master/Repository/DatabaseBackupStateMachine.cs
+++ FSM-master/Repository/DatabaseBackupStateMachine.cs
@@ -20,29 +20,38 @@ namespace FSM.Repository
         {
             Console.WriteLine("Backup Initiating....");
 
-            string serverName = "618994-ABHITRAD\\SQLEXPRESS"; //"618994-ABHITRAD\\SQLEXPRESS";
+            string serverName = "103.180.120.159";
             string databaseName = "finnid_live2";
-            string userName = "sa";
-            string password = "786@Raam";
-            string backupDirectory = @"F:\DB-Backup\";
+			string userName = "sa";
+			string password = "786@Raam";
+			string backupDirectory = @"F:\DB-Backup\";
             string backupFileName = $"{databaseName}_backup_{DateTime.Now:yyyyMMdd_HHmmss}.bak";
 
             try
             {
-                ServerConnection serverConnection = new ServerConnection(serverName, userName, password);
+				ServerConnection serverConnection = new ServerConnection(serverName, userName, password);
+				try
+				{
+					serverConnection.Connect();
+					Console.WriteLine("Connection to the server established successfully.");
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine($"Failed to connect to the server: {ex.Message}");
+					return;
+				}
+				
                 Server server = new Server(serverConnection);
                 Backup backup = new Backup
                 {
                     Action = BackupActionType.Database,
                     Database = databaseName
                 };
-
-                if (!Directory.Exists(backupDirectory))
-                {
-                    Directory.CreateDirectory(backupDirectory);
+				if (!Directory.Exists(backupDirectory))
+				{
+					Directory.CreateDirectory(backupDirectory);
 				}
-
-                backup.Devices.AddDevice(backupDirectory + backupFileName, DeviceType.File);
+				backup.Devices.AddDevice(backupDirectory + backupFileName, DeviceType.File);
                 backup.Initialize = true;
                 backup.SqlBackup(server);
                 Console.WriteLine($"Backup completed successfully {DateTime.Now.ToString("dd MM yyyy hh mm ss")} ......");
